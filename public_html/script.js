e9ce5bbf1a87cfcc3eca8faea342bc6e0b63b5b2
Brandi Meza <bmeza2@cnm.edu>
2021-12-07 00:37:24 -0700

    WIP

--- public_html/script.js
+++ public_html/script.js
@@ -1,3 +1,42 @@
+// navbar
+
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load', function (){
+
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Contact Form
 $(document).ready(function(){
     document.getElementById("contact")
     $("#contact").validate({
